8d20001d1d06871f11df46ca1197b43451ba0e8e
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-05-04 10:32:26 +0530

    adding header and footer to error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import Footer from "./Footer";
+import MultiLevelNavbar from "./MultiLevelNavbar";
 
-const Error = ()=>{
-    return(
-        <div className="h-[100vh] w-full flex justify-center items-center">
-            <h1 className="text-4xl text-center mb-20 text-red-500"> 😔 No Such Place Found! </h1>
-        </div>
-    )
-}
+const Error = () => {
+  return (
+    <>
+      <MultiLevelNavbar />
+      <div className="h-[100vh] w-full flex justify-center items-center">
+        <h1 className="text-4xl text-center mb-20 text-red-500">
+          {" "}
+          😔 No Such Place Found!{" "}
+        </h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Error;
